7be4e6527de3dc6ae0ea19397ad7810240e9b6df
Muhindhar <muhindharsv27@gmail.com>
2026-07-06 20:21:14 +0530

    added blood component feature in billing

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src\\test\\resources\\features\\Billing\\BloodComponent.feature",
+		features = "src\\test\\resources\\features",
 		glue = "definitions",
 		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
